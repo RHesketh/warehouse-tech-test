0f29df85536e46595dae1fe1ede043f1152bd626
Robert Hesketh <rhesketh.uk@gmail.com>
2024-09-11 17:38:33 +0100

    Added front page for viewing unverified scans

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -2,6 +2,10 @@
 
 # Incoming scan data from Robot warehouse scanners
 class ScansController < ApplicationController
+  before_action :set_scans, only: %i[index]
+
+  def index; end
+
   def create
     Scan.create_from_json(report_file: scan_params['file'])
     head :created
@@ -10,6 +14,12 @@ class ScansController < ApplicationController
   end
 
   private
+
+  def set_scans
+    @scans = Scan.all.includes(:comparison).order(created_at: :desc)
+    @verified_scans = @scans.where.not(comparison: { scan_id: nil })
+    @unverified_scans = @scans.where(comparison: { scan_id: nil })
+  end
 
   def scan_params
     params.require(:scan).permit(:file)
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :scans, only: %i[create]
+  root "scans#index"
+  resources :scans, only: %i[index create]
 end
